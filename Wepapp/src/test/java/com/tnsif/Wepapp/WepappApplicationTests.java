32f296c8baef16d4ec02204773062da176df9e3a
Sri <Sri@LAPTOP-C4QF4B05.mshome.net>
2025-07-11 12:37:06 +0530

    webapp programs

---
+++ Wepapp/src/test/java/com/tnsif/Wepapp/WepappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tnsif.Wepapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WepappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
