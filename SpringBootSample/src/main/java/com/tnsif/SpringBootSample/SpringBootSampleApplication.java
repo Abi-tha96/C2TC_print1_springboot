ebfaafb2cf33fd4a014c9b7d71160e0380c4128c
Sri <Sri@LAPTOP-C4QF4B05.mshome.net>
2025-07-11 12:35:48 +0530

    spring boot example projects

---
+++ SpringBootSample/src/main/java/com/tnsif/SpringBootSample/SpringBootSampleApplication.java
@@ -0,0 +1,14 @@
+package com.tnsif.SpringBootSample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSampleApplication.class, args);
+		System.out.println("Hello world");
+	}
+
+}
